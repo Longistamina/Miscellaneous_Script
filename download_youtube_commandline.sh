ff5a7522023e074e552fae0ebd96704cca4cbcb1
Longistamina <dophamthelong03@gmail.com>
2025-10-15 20:54:42 +0900

    added contents to download youtube

--- download_youtube_commandline.sh
+++ download_youtube_commandline.sh
@@ -26,3 +26,15 @@ yt-dlp --merge-output-format mp4 -f "bestvideo+bestaudio" -o "/home/longdpt/Vide
 # output in /home/longdpt/Videos/
 # Input url is: https://youtu.be/m3PguKYQXAY
 
+
+#----------------------------------------------------------------------------------------------#
+#------------- download youtube with some restrictions (like Age verification -----------------#
+#----------------------------------------------------------------------------------------------#
+
+# Install secretstorage to help bypass cookies
+python3 -m pip install secretstorage
+
+# Download with --cookies-from-browser [browser name]
+yt-dlp --cookies-from-browser brave --merge-output-format mp4 -f "bestvideo+bestaudio" -o "/home/longdpt/Videos/%(title)s.%(ext)s" https://www.youtube.com/watch?v=5zYBZi34bDo
+
+# --cookies-from-browser brave (brave is the browser name, if you google chrome => chrome)
